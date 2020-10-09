299ffa057d0cc33a44ebaf04ad8dec2e640c8019
Amy Fabella <amyfabella@hotmail.com>
2020-10-09 16:39:16 -0700

    fixed footer

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Lander from './pages/lander/'
 import Home from './pages/home/'
 import ShowCase from './pages/showcase/'
 import Video from './pages/video/'
-import Footer from './components/Footer'
+import Footer from './components/footer'
 import './App.css';
 import {Container} from 'reactstrap';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
--- client/src/pages/about/index.js
+++ client/src/pages/about/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container, Col, Row } from 'reactstrap'
-import Header from '../../components/Header';
-import Main from '../../components/Main';
+import Header from '../../components/header';
+import Main from '../../components/main';
 const About = () => {
     return (
         <>
