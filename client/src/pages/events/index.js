4db4b426b82544da1757931c8deef93bd0e23d91
hschaen <hschaen@gmail.com>
2020-10-12 17:13:09 -0700

    redid homepage w bootstrap

---
+++ client/src/pages/events/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container, Col, Row } from 'reactstrap'
+import Header from '../../components/Header';
+import Main from '../../components/Main';
+const About = () => {
+    return (
+        <>
+            <Col>
+                <Header>The House Entertainment</Header>
+                <Main className="center">
+                    <p className={"white"}>The House Entertainmnet is an entertainment services 
+                    <br/>and content creation company based out of San Diego, CA.</p>
+                    <p className={"gray"}>For more information contact: <a href="mailto:hello@thehouseentertainment.com">hello@thehouseentertainment.com</a></p>
+                </Main>
+            </Col>
+        </>
+    )
+}
+export default About
